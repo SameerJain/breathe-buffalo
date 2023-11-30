140dd8f98ff22d85a35f656568a3d4ddf12c6199
SameerJain <114765510+SameerJain@users.noreply.github.com>
2023-11-30 09:28:30 -0500

    x

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import locationIcon from '@iconify/icons-mdi/fire-alert' //import the specific f
 const header = () => {
   return (
     <header className="header">
-        <h1><Icon icon ={locationIcon} /> UB Air Quality Buddy </h1>
+        <h1><Icon icon ={locationIcon} /> Breathe Buffalo </h1>
     </header>
   )
 }
